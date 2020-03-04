e0b7b882ed850a5dedcf2d6cbcb5952ccb2a0789
Heber Quequejana <heberqc@gmail.com>
2020-03-04 16:16:19 -0500

    add cell component

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import Circle from './Circle';
 
-export default function Cell(props) {
-	let style = {
-		height: 50,
-		width: 50,
-		border: '1px solid black',
-		backgroundColor: 'yellow'
-	};
-	return (
-		<div style={style} onClick={() => { props.handleClick(props.row, props.col) }}>
-			<Circle cell={props.cell} />
-		</div>
-	);
-}
+const StyledCell = styled.div`
+	height: 50px;
+	width: 50px;
+	border: 1px solid black;
+	background-color: yellow;
+`
+
+const Cell = ({row, col, cell, handleClick}) => (
+	<StyledCell onClick={() => handleClick(row, col)}>
+		<Circle cell={cell} />
+	</StyledCell>
+)
+
+export default Cell;
