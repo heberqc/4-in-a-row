3ba222c28a2fff36f2e2e450d2ef986cd08f887f
Heber Quequejana <heberqc@gmail.com>
2020-03-05 17:13:04 -0500

    load game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,88 +1,82 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import Board from './Board';
 
-class Game extends Component {
-	constructor(props) {
-		super(props);
-		let cells = [];
-		for (let i = 0; i < 6; i++) {
-			cells.push(new Array(7).fill(0));
-		}
-		this.state = { player: false, cells: cells, winner: 0 };
-		this.handleClick = this.handleClick.bind(this);
-	}
+const Game = () => {
+	const [player, setPlayer] = useState(false);
+	const [cells, setCells] = useState(Array(6).fill(null).map((() => Array(7).fill(0))));
+	const [winner, setWinner] = useState(0);
 
-	handleClick(row, col) {
-		if (this.state.winner) return;
+	const handleClick = (row, col) => {
+		if (winner) return;
 		console.log(`row: ${row} | col: ${col}`);
-		console.log(this.state.player);
+		console.log(player);
 		let temp = [];
 		for (let i = 0; i < 6; i++) {
-			temp.push(this.state.cells[i].slice());
+			temp.push(cells[i].slice());
 		}
-		let newRow = this.findAvailableRow(col);
-		temp[newRow][col] = this.state.player ? 1 : 2;
-		this.setState({ cells: temp, player: !this.state.player }, () => {
-			if (this.checkVictory(newRow, col)) {
-				console.log('win');
-				this.setState({ winner: this.state.player ? 2 : 1 });
-			}
-		});
+		let newRow = findAvailableRow(col);
+		temp[newRow][col] = player ? 1 : 2;
+		setCells(temp);
+		setPlayer(!player);
+		if (checkVictory(newRow, col)) {
+			console.log('win');
+			setWinner(player ? 2 : 1);
+		}
 	}
 
-	findAvailableRow(col) {
+	const findAvailableRow = (col) => {
 		for (let i = 0; i < 6; i++) {
-			if (this.state.cells[i][col] === 0) {
+			if (cells[i][col] === 0) {
 				return i;
 			}
 		}
 		return -1;
 	}
 
-	checkVictory(row, col) {
-		let player = this.state.player ? 2 : 1;
-		let board = this.state.cells;
+	const checkVictory = (row, col) => {
+		let player_n = player ? 2 : 1;
+		let board = cells;
 		let contador = 0;
 		// horizontal
 		for (let i = 0; col + i < 7; i++) {
-			if (board[row][col + i] === player) contador++;
+			if (board[row][col + i] === player_n) contador++;
 			else break;
 		}
 		for (let i = 1; col - i >= 0; i++) {
-			if (board[row][col - i] === player) contador++;
+			if (board[row][col - i] === player_n) contador++;
 			else break;
 		}
 		if (contador >= 4) return true;
 		else contador = 0;
 		// positive diagonal
 		for (let i = 0; row + i < 6 && col + i < 7; i++) {
-			if (board[row + i][col + i] === player) contador++;
+			if (board[row + i][col + i] === player_n) contador++;
 			else break;
 		}
 		for (let i = 1; row - i >= 0 && col - i >= 0; i++) {
-			if (board[row - i][col - i] === player) contador++;
+			if (board[row - i][col - i] === player_n) contador++;
 			else break;
 		}
 		if (contador >= 4) return true;
 		else contador = 0;
 		// negative diagonal
 		for (let i = 0; row + i < 6 && col - i >= 0; i++) {
-			if (board[row + i][col - i] === player) contador++;
+			if (board[row + i][col - i] === player_n) contador++;
 			else break;
 		}
 		for (let i = 1; row - i >= 0 && col + i < 7; i++) {
-			if (board[row - i][col + i] === player) contador++;
+			if (board[row - i][col + i] === player_n) contador++;
 			else break;
 		}
 		if (contador >= 4) return true;
 		else contador = 0;
 		// vertical
 		for (let i = 0; row + i < 6; i++) {
-			if (board[row + i][col] === player) contador++;
+			if (board[row + i][col] === player_n) contador++;
 			else break;
 		}
 		for (let i = 1; row - i >= 0; i++) {
-			if (board[row - i][col] === player) contador++;
+			if (board[row - i][col] === player_n) contador++;
 			else break;
 		}
 		if (contador >= 4) return true;
@@ -90,23 +84,19 @@ class Game extends Component {
 		return false;
 	}
 
-	restart() {
-		let cells = [];
-		for (let i = 0; i < 6; i++) {
-			cells.push(new Array(7).fill(0));
-		}
-		this.setState({ player: false, cells: cells, winner: 0 });
+	const restart = () => {
+		setPlayer(false);
+		setCells(cells);
+		setWinner(Array(6).fill(null).map((() => Array(7).fill(0))));
 	}
 
-	render() {
-		return (
-			<div>
-				<h1>{this.state.winner > 0 ? this.state.winner === 1 ? 'Black Wins' : 'Red Wins' : this.state.player ? 'Blacks Turn' : 'Reds Turn'} </h1>
-				<Board cells={this.state.cells} handleClick={this.handleClick} />
-				<button onClick={() => this.restart()}>Restart</button>
-			</div>
-		);
-	}
-}
+	return (
+		<>
+			<h1>{winner > 0 ? winner === 1 ? 'Black Wins' : 'Red Wins' : player ? 'Blacks Turn' : 'Reds Turn'} </h1>
+			<Board cells={cells} handleClick={handleClick} />
+			<button onClick={() => restart()}>Restart</button>
+		</>
+	)
+};
 
 export default Game;
